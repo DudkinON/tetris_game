d1be3ad151189ef8809fe7f22bbba93a9ba0f5fa
Oleg Dudkin <dudkin.on@gmail.com>
2018-05-12 04:35:47 -0500

    feat: Define variables

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,15 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Arrays;
+
 class Game extends JFrame {
+
+    private int gameScore = 0;
+    public static int[][] canv = new int[GameSettings.FIELD_HEIGHT + 1][GameSettings.FIELD_WIDTH];
+    private Canvas canvas = new Canvas();
+    private static Figure figure = new Figure();
+    private static boolean gameOver = false;
+
 
 }
