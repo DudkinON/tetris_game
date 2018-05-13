09a6514ed445cb548389a507f367b42d6ae6ebac
Oleg Dudkin <dudkin.on@gmail.com>
2018-05-13 04:19:12 -0500

    feat: Add constructor to Figure class

--- src/Figure.java
+++ src/Figure.java
@@ -4,4 +4,35 @@ import java.util.Random;
 
 class Figure {
 
+    private ArrayList<Block> figure = new ArrayList<>();
+    private int[][] shape = new int[4][4];
+    private int size;
+    private int color;
+    private int x = 3, y = 0;
+
+    // Create constructor
+    Figure() {
+
+        // Create random object
+        Random random = new Random();
+
+        // Define random integer in shapes array length range
+        int type = random.nextInt(Shape.SHAPES.length);
+
+        // Get size
+        size = Shape.SHAPES[type][4][0];
+
+        // Get a shape
+        color = Shape.SHAPES[type][4][1];
+
+        // If size 4 change start coordinate
+        if (size == 4) y = -1;
+
+        for (int i = 0; i < size; i++) {
+            System.arraycopy(Shape.SHAPES[type][i], 0, shape[i], 0, Shape.SHAPES[type][i].length);
+        }
+
+        createFromShape();
+    }
+
 }
